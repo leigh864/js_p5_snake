47d5c13a5008e9a6515a3b15cfe8f3916b4e9929
leigh864 <leigh864@btinternet.com>
2018-12-31 15:59:12 +0000

    food being eaten and regenned and score increased

--- sketch.js
+++ sketch.js
@@ -1,30 +1,34 @@
 // global variables
-var canvasWidth = 200;
-var canvasHeight = 200;
-var snakeSize = 20;
-var halfSize = Math.floor(snakeSize / 2);
+var canvasWidth = 600;
+var canvasHeight = 600;
+var itemSize = 30;
+var halfSize = Math.floor(itemSize / 2);
+var doubleSize = itemSize * 2;
+var oneQSize = itemSize * 0.25;
+var threeQSize = itemSize * 0.75;
 var startX = halfSize;
 var startY = halfSize;
 var sid;
 var gameScore = 0;
 var direction = 1;
-
+var foodSeed = true;
+var foodScore = 10000;
 var gameState = true;
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	sid = new Snakey(startX, startY);
 
-	frameRate(30);
+	// frameRate(30);
 }
 
 function draw() {
 	if (gameState) {
 		background(51);
-
 		sid.update();
 		sid.snakeCrash();
 		sid.show();
+		sid.eaten();
 	} else {
 		alert('You scored ' + gameScore);
 		remove();
