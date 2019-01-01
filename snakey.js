7554912612f9daec9e4cbf9257ee602c2547e935
leigh864 <leigh864@btinternet.com>
2019-01-01 14:37:45 +0000

    console removed and cleanup

--- snakey.js
+++ snakey.js
@@ -22,12 +22,10 @@ class Snakey {
 	drawSnake() {
 		fill('#0f0');
 		ellipse(this.x, this.y, itemSize, itemSize);
-
 		for (let i = 0; i < this.tail; i++) {
 			fill('#0f0');
 			ellipse(this.moves[i].x, this.moves[i].y, itemSize, itemSize);
 		}
-
 		textSize(sizeText);
 		text('Score=' + gameScore + '  tail=' + this.tail, 5, sizeText);
 	}
@@ -66,10 +64,6 @@ class Snakey {
 	updateTail() {
 		if (this.moves.length > this.tail + 1) {
 			this.moves.pop();
-			// console.log('>>>>>>>>>>>>> tail=' + this.tail);
-			// for (let i = 0; i < this.moves.length; i++) {
-			// 	console.log('i=', i, ' moves=' + JSON.stringify(this.moves[i]));
-			// }
 		}
 
 		counter++;
@@ -77,12 +71,6 @@ class Snakey {
 			this.moves.unshift({ x: this.x, y: this.y });
 			counter = 0;
 		}
-
-		// console.log(
-		// 	`counter=${counter} itemSize=${itemSize} this.tail=${
-		// 		this.tail
-		// 	} this.moves.length=${this.moves.length}`
-		// );
 	}
 
 	snakeCrash() {
@@ -96,35 +84,15 @@ class Snakey {
 			gameState = false;
 		}
 
-		// check not hit the tail - there is await of 30 pixels between
-		// moves - so dont check till we get the next one, as seomtimes tail is waiting for a 'full move' before adding the segment (its part way through)
 		for (let i = 2; i < this.tail && this.tail <= this.moves.length; i++) {
-			// console.log(`i ${i} this.tail ${this.tail}`);
 			var collision = dist(this.x, this.y, this.moves[i].x, this.moves[i].y);
-
 			if (collision < itemSize) {
-				console.log(
-					'>>>>>>>>>> ****BOOM***** collision = ',
-					collision,
-					' i was ',
-					i,
-					' itemSize=',
-					itemSize
-				);
-				console.log(`this.x	${this.x}`);
-				console.log(`this.y ${this.y}`);
-				console.log(`this.moves[i].x ${this.moves[i].x}`);
-				console.log(`this.moves[i].y ${this.moves[i].y}`);
-				for (let i = 0; i < this.moves.length; i++) {
-					console.log('i', i, 'moves=' + JSON.stringify(this.moves[i]));
-				}
 				gameState = false;
 			}
 		}
 		if (gameState) gameScore++;
 	}
 
-	// x and y are at centre of circle
 	checkEdge() {
 		this.shellX = this.x;
 		this.shellY = this.y;
@@ -149,15 +117,9 @@ class Snakey {
 
 	eaten() {
 		var distance = dist(this.x, this.y, this.foodX, this.foodY);
-		// console.log(distance);
 		if (distance < itemSize + 1) {
 			this.tail++;
-			console.log('EATEN - YUMMY - >>>>>>>>>>>>>>>>>>> tail=', this.tail);
-			for (let i = 0; i < this.moves.length; i++) {
-				console.log('i', i, 'moves=' + JSON.stringify(this.moves[i]));
-			}
 			gameScore += foodScore;
-			console.log(`gamescore=${gameScore}`);
 			foodSeed = true;
 		}
 	}
